fd20229908fd441d080f954ad6611b5051dc04a8
Megan Buck <thequiltingriverotter@gmail.com>
2023-12-28 03:35:00 -0500

    Changed route for signup page on router.jsx

--- front_end/dino_stocks/src/router.jsx
+++ front_end/dino_stocks/src/router.jsx
@@ -1,44 +1,45 @@
-import {createBrowserRouter} from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
 import App from "./App"
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import MarketPage from "./pages/MarketPage";
 import OverviewPage from "./pages/OverviewPage";
-import SignUpPage from "./pages/Signuppage";
+import SignUpPage from "./pages/SignUpPage";
 import StockDetailPage from "./pages/StockDetailPage";
 import NotFoundPage from "./pages/NotFoundPage";
 const router = createBrowserRouter([
-        {path:"/",
-    element: <App/>,
-    children:[
-        {
-            index:true,
-            element: <HomePage/>
-        },
-        {
-            path: "login/",
-            element: <LoginPage/>
-        },
-        {
-            path: "market/",
-            element: <MarketPage/>
-        },
-        {
-            path: "overview/",
-            element: <OverviewPage/>
-        },
-        {
-            path: "signup/",
-            element: <SignUpPage/>
-        },
-        {
-            path:"stock/:id/",
-            element: <StockDetailPage/>
-        }
-        
-    ],
-    errorElement: <NotFoundPage/>
-},
-    ])
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: "login/",
+                element: <LoginPage />
+            },
+            {
+                path: "market/",
+                element: <MarketPage />
+            },
+            {
+                path: "overview/",
+                element: <OverviewPage />
+            },
+            {
+                path: "signup/",
+                element: <SignUpPage />
+            },
+            {
+                path: "stock/:id/",
+                element: <StockDetailPage />
+            }
 
-    export default router;
+        ],
+        errorElement: <NotFoundPage />
+    },
+])
+
+export default router;
