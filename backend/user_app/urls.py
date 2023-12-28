f34794ffa06551185f3d84f3713ac0edc7d42ccd
Cody Hardy <chardy0919@gmail.com>
2023-12-28 11:40:56 -0500

    added getInfo for user model

--- backend/user_app/urls.py
+++ backend/user_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUp, LogIn, LogOut
+from .views import Info, SignUp, LogIn, LogOut
 
 urlpatterns=[
+    path('', Info.as_view(), name='Info'),
     path('SignUp/', SignUp.as_view(), name='SignUp'),
     path('LogIn/', LogIn.as_view(), name='LogIn'),
     path('LogOut/', LogOut.as_view(), name='LogOut')
