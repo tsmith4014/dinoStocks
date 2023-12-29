37d41e302308ae79a100212113236c4ca7a333c5
Megan Buck <thequiltingriverotter@gmail.com>
2023-12-28 23:08:04 -0500

    Added view for historicals_app

--- backend/historicals_app/views.py
+++ backend/historicals_app/views.py
@@ -6,12 +6,22 @@ from .serializers import HistoricalSerializer
 from portfolio_app.models import Portfolio
 from portfolio_app.serializers import PortfolioSerializer
 from realstonks_app.models import StockMarket
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
+
 
 class HistoricalInfo(APIView):
     def get(self, request):
-        # historical_obj = HistoricalSerializer(request.user.portfolio.historicals)
-        # print(historical_obj.data)
-        # return Response(historical_obj.data)
-        # pass
-       
-        return Response(True)
+        try:
+            portfolio = request.user.portfolio
+            historicals = Historicals.objects.filter(portfolio_id=portfolio).order_by(
+                "time_stamp"
+            )
+            ser_historicals = HistoricalSerializer(historicals, many=True)
+            return Response(ser_historicals.data, status=HTTP_204_NO_CONTENT)
+        except:
+            return Response("Historical data not found", status=HTTP_404_NOT_FOUND)
--- backend/portfolio_app/serializers.py
+++ backend/portfolio_app/serializers.py
@@ -2,19 +2,22 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Portfolio
 from shares_app.serializers import SharesSerializer
+from historicals_app.serializers import HistoricalSerializer
+
 
 class PortfolioSerializer(ModelSerializer):
     def get_shares_information(self, instance):
         shares = instance.shares.all()
         return SharesSerializer(shares, many=True).data
 
-    shares = serializers.SerializerMethodField(method_name='get_shares_information')
-# import stock serializer and create serializer method field
+    def get_historicals(self, instance):
+        historicals = instance.historicals.all()
+        return HistoricalSerializer(historicals, many=True).data
+
+    shares = serializers.SerializerMethodField(method_name="get_shares_information")
+    historicals = serializers.SerializerMethodField(method_name="get_historicals")
+
+    # import stock serializer and create serializer method field
     class Meta:
         model = Portfolio
-        fields = [
-            'user',
-            'id',
-            'money',
-            'shares'
-        ]
+        fields = ["user", "id", "money", "shares", "historicals"]
