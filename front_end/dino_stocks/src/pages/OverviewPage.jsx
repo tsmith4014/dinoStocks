621437a9a25b8ea0b8a621780e6d0598757ae1d2
Megan Buck <thequiltingriverotter@gmail.com>
2024-01-02 02:51:20 -0500

    minor changes

--- front_end/dino_stocks/src/pages/OverviewPage.jsx
+++ front_end/dino_stocks/src/pages/OverviewPage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Card, Table } from 'react-bootstrap';
+import PortfolioLineChart from '../components/PortfolioLineChart';
 import axios from 'axios';
 import { userAPI } from '../utilities';
+
 const OverviewPage = () => {
   const [userInfo, setUserInfo] = useState([])
 
@@ -10,6 +12,51 @@ const OverviewPage = () => {
     { stock: 'Jurassic Ventures', details: '10 shares @ $80', price: '$800' },
     { stock: 'Prehistoric Inc', details: '5 shares @ $120', price: '$600' },
   ];
+
+  const data = [
+    {
+      "name": "Page A",
+      "uv": 4000,
+      "pv": 2400,
+      "amt": 2400
+    },
+    {
+      "name": "Page B",
+      "uv": 3000,
+      "pv": 1398,
+      "amt": 2210
+    },
+    {
+      "name": "Page C",
+      "uv": 2000,
+      "pv": 9800,
+      "amt": 2290
+    },
+    {
+      "name": "Page D",
+      "uv": 2780,
+      "pv": 3908,
+      "amt": 2000
+    },
+    {
+      "name": "Page E",
+      "uv": 1890,
+      "pv": 4800,
+      "amt": 2181
+    },
+    {
+      "name": "Page F",
+      "uv": 2390,
+      "pv": 3800,
+      "amt": 2500
+    },
+    {
+      "name": "Page G",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    }
+  ]
 
   const fetchPortfolio = async () => {
     let token = localStorage.getItem("token")
@@ -67,8 +114,8 @@ const OverviewPage = () => {
             <Card.Body>
               <Card.Title>Graph with Portfolio Performance</Card.Title>
               {/* Placeholder for graph component */}
-              <div style={{ height: '300px', backgroundColor: '#f0f0f0' }}>
-                Portfolio Performance Graph Here
+              <div >
+                <PortfolioLineChart data={data} />
               </div>
             </Card.Body>
           </Card>
