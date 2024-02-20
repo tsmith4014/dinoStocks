dbf857318f78ccbd276066e4dfb8aec21be9abc5
CoderBob <mcjames1986@gmail.com>
2024-02-20 09:20:52 -0500

    first commit

--- front_end/dino_stocks/src/utilities.jsx
+++ front_end/dino_stocks/src/utilities.jsx
@@ -1,5 +1,10 @@
-import axios from "axios"
+import axios from "axios";
+
+const host = process.env.REACT_APP_BACKEND_HOST || "127.0.0.1";
+const port = process.env.REACT_APP_BACKEND_PORT || "8000";
+
+const baseURL = `http://${host}:${port}/api/v1/user/`;
 
 export const userAPI = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/v1/user/"
-})
+    baseURL: baseURL,
+});
