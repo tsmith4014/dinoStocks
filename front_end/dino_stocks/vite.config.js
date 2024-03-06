bb1157fda524e994540a28df6b57cd7853fa2093
tsmith4014 <chjthomps@gmail.com>
2024-03-06 15:13:34 -0500

    updated to final build with eks rdy front/backend/celery/k8s/dockerfiles/docker-compose

--- front_end/dino_stocks/vite.config.js
+++ front_end/dino_stocks/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host:"0.0.0.0",
-    port:5173,
-  },
   plugins: [react()],
 })
